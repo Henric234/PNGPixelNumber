840f5a3d6e5a703074c21ba3d09a403e855fc795
henric234 <helps2001@gmail.com>
2020-08-04 13:29:54 -0300

    main looping through all files in the images folder and giving result

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,23 @@
 namespace fs = std::filesystem;
 
 int main() {
-    std::string filename = "../data/images/deslocamento.PNG";
+    const std::string path = "../data/images/";
+    std::string filename;
     std::vector<unsigned char> buffer;
     std::vector<unsigned char> image;
     unsigned w, h;
 
-    lodepng::load_file(buffer, filename); //load the image file with given filename
+    for (const auto & entry : fs::directory_iterator(path)) {
+        filename = entry.path().filename();
+        lodepng::load_file(buffer, entry.path());
+        unsigned error = lodepng::decode(image, w, h, buffer);
 
-    lodepng::State state;
+        if(error) {
+            std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+            return 0;
+        }
 
-    unsigned error = lodepng::decode(image, w, h, state, buffer);
-
-    if(error) {
-        std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
-        return 0;
+        std::cout << filename << " = " << w * h << std::endl;
     }
-
-    std::cout << "Num pixels: " << w * h << std::endl;
-
     return 0;
 }
